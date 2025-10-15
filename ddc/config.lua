4f8ca5413342b5aef255b16e57ea7ca931d3b45a
keyhoh <iheyhoh+github@gmail.com>
2025-10-15 15:22:38 +0900

    update: lsp config

--- ddc/config.lua
+++ ddc/config.lua
@@ -3,6 +3,7 @@ require('ddc_source_lsp_setup').setup()
 local lsp_list = vim.g.lsp_list or {}
 for _, lsp in pairs(lsp_list) do
   local options = lsp.options or {}
-  require('lspconfig')[lsp.name].setup(options)
+  vim.lsp.config(lsp.name, options)
+  vim.lsp.enable(lsp.name)
 end
 
